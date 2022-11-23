1dd4da06bcc7a2521768117788f6654553f63381
Liam Robertson <liam2robertson@gmail.com>
2022-11-23 15:23:34 +0000

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/items" element={<h1>Items</h1>} />
+          <Route path="/checkout" element={<h1>Checkout</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
