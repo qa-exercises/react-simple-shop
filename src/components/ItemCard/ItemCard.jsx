e4476e5c6d6d2be15ef252f79a9c4032e4ab8962
Liam Robertson <liam2robertson@gmail.com>
2022-11-25 10:41:04 +0000

    specify width and height for images

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -11,7 +11,7 @@ const ItemCard = ({ id, name, price, imageUrl }) => {
 
   return (
     <div className={styles.wrapper}>
-      <img src={imageUrl} width={200} alt={name} title={name} />
+      <img src={imageUrl} width={200} height={250} alt={name} title={name} />
       <h3 className={styles.name}>{name}</h3>
       <p className={styles.price}>£{price.toFixed(2)}</p>
       {isInCart
