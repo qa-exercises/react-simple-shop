a1ddc8fd09926d94e8cd7930c57ae48f1b447032
Liam Robertson <liam2robertson@gmail.com>
2022-11-25 10:23:44 +0000

    add logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLeaf } from '@fortawesome/free-solid-svg-icons'
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <h2 className={styles.header}>seed 🌱 theory</h2>
+      <h2 className={styles.header}>
+        seed <FontAwesomeIcon icon={faLeaf} /> theory
+      </h2>
       <div className={styles['nav-link-wrapper']}>
         <Link to="/">Home</Link>
         <Link to="/items">Shop</Link>
