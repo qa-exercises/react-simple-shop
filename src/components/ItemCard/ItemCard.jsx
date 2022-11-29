23ab6b4f5a1320e314b7f9853664a85d53509e97
Liam Robertson <liam2robertson@gmail.com>
2022-11-29 15:42:31 +0000

    simplify ItemCard component

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -14,17 +14,12 @@ const ItemCard = ({ id, name, price, imageUrl }) => {
       <img src={imageUrl} width={200} height={250} alt={name} title={name} />
       <h3 className={styles.name}>{name}</h3>
       <p className={styles.price}>£{price.toFixed(2)}</p>
-      {isInCart
-        ? (
-          <button onClick={removeFromCart} className={styles['remove-from-cart']}>
-            In cart
-          </button>
-        )
-        : (
-          <button onClick={addToCart} className={styles['add-to-cart']}>
-            Add to cart
-          </button>
-        )}
+      <button
+        onClick={isInCart ? removeFromCart : addToCart}
+        className={styles[isInCart ? 'remove-from-cart' : 'add-to-cart']}
+      >
+        {isInCart ? 'In cart' : 'Add to cart'}
+      </button>
     </div>
   )
 }
