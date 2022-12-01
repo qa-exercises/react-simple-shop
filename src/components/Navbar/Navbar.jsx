fbd3446f4bd3acbde6710fce6e3caeb1d5421006
Liam Robertson <liam2robertson@gmail.com>
2022-12-01 13:16:43 +0000

    remove logo from navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,11 @@
 import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLeaf } from '@fortawesome/free-solid-svg-icons'
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <h2 className={styles.header}>
-        seed <FontAwesomeIcon icon={faLeaf} /> theory
+        seed theory
       </h2>
       <div className={styles['nav-link-wrapper']}>
         <Link to="/">Home</Link>
