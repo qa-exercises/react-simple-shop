f86a7efcefb3c2e16dfeaf0e410ee42dcade95d7
Liam Robertson <liam2robertson@gmail.com>
2022-12-02 18:18:58 +0000

    text change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
       <div>
         <h1>Bring your<br/>home to life.</h1>
         <p>Elevate your living space with a touch of nature.</p>
-        <p>We're sure you'll love our selection of houseplants.</p>
+        <p>We offer a range of easy-care houseplants.</p>
         <Link to="/items">Browse plants</Link>
       </div>
       <img src={homeImageUrl} alt="House plants" height={500} width={390} />
